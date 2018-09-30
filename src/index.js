287660f52cf789df2990857907adcc55fa997a63
VladikRebko <rebko.v.v@mail.com>
2018-09-30 17:48:03 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	
+	let countOfZeros = 0;
+
+	while(number > 0){
+		number = Math.floor(number / 5);
+		countOfZeros += number;
+	}
+
+	return countOfZeros;
+
 }
